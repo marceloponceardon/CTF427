3a17586b1f4e7c6f902e5995c2a53301d6a23c34
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2025-02-26 16:15:09 -0500

    sessions

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const path = require("path");
 const md5 = require("md5");
+const session = require("express-session");
 
 const client = require("./db");
 
 const app = express();
 
+// public
 app.use(express.static(path.join(__dirname, "public")));
-
+// sessions
+app.use(session({
+	secret: process.env.SESSION_SECRET || "super-secret",
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: process.env.NODE_ENV === 'production' }
+}));
+// body parser
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -24,8 +33,20 @@ app.post("/login", async (req, res) => {
 	// Use an easily breakable hash function
 	const password_hashed = md5(password);
 	const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password_hashed}'`;
-	console.log(query);
+
+	// Execute the query
+	console.log("Query:", query);
+	// TODO: Actually do the query
+	
 });
+
+function auth(req, res, next) {
+	if (req.session.auth) {
+		next();
+	} else {
+		res.status(401).send("Unauthorized >:(. Please <a href='/login'>login</a> first.");
+	}
+}
 
 const port = process.env.PORT || 3000;
 // TODO: Change this if we want more hints to be around
