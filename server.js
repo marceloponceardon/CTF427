9a501f6be03e70f211ac16b0c243f617119c4380
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2025-03-03 20:07:02 +0000

    fix for dockerizing

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const pool = new pg.Pool({
 	password: process.env.DB_PASS,
 	host: process.env.DB_HOST,
 	database: process.env.DB_NAME,
-	port: 5432
+	port: 5432,
+	ssl: { rejectUnauthorized: false },
 });
 
 // Test connection
